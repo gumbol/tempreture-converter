cbca6aea10efc90c2766422d24c8fa9a429f3232
gumbol <gumbol@github.io>
2021-02-28 15:20:04 +0300

    ini local commit

---
+++ js/class-tempreture.js
@@ -0,0 +1,24 @@
+class Temperature {
+  constructor(temp) {
+    this.c = (temp - 32) / 1.8;
+    // this.temp = temp;
+    this.f = temp * 1.8 + 32;
+  }
+
+  set toFahrenheit(c) {
+    this.c = c;
+    this.f = Math.round(c * 1.8 + 32);
+  }
+  get fahrenheit() {
+    return this.f;
+  }
+
+  set toCelsius(f) {
+    this.f = f;
+    this.c = Math.round((f - 32) / 1.8);
+
+  }
+  get celsius() {
+    return this.c;
+  }
+}
